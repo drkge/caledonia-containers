8f68ec9c2e664c10db30ad71ca66f55479c1cf12
Derek <191865662+drkgr@users.noreply.github.com>
2026-07-13 11:17:33 +0100

    added new files

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Skip Manufacturer UK";
+export const SITE_TITLE = "Caledonia Containers";
 export const SITE_DESCRIPTION =
-  "Looking for skips, repairs, or custom metalwork? Skip Manufacturers UK offers fabrication, shot blasting, welding, painting, & more for skips, containers, trucks, & more! Fast turnaround, quality work.";
+  "Looking for skips, repairs, or custom metalwork? Caledonia Containers offers fabrication, shot blasting, welding, painting, & more for skips, containers, trucks, & more! Fast turnaround, quality work.";
